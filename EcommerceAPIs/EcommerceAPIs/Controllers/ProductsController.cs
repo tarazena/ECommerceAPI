9eabe706b5a6db21f27f5f9c6809a3d69f636568
tarazena <tarazena@gmail.com>
2017-12-11 16:37:14 -0500

    initial API

---
+++ EcommerceAPIs/EcommerceAPIs/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using EcommerceAPIs.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace EcommerceAPIs.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        [HttpGet]
+        [Route("v1/getproductlist")]
+        public HttpResponseMessage GetProductList()
+        {
+            List<Product> products = new List<Product>();
+
+            products = LoadJson();
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, products);
+            response.Headers.Add("Access-Control-Allow-Origin", "http://localhost:8080");
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+            
+            return response;
+        }
+
+
+        public List<Product> LoadJson()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "EcommerceAPIs.Static.product-items.json";
+
+
+            List<Product> items = new List<Product>();
+            string dir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader r = new StreamReader(stream))
+            {
+                string json = r.ReadToEnd();
+                items = JsonConvert.DeserializeObject<List<Product>>(json);
+            }
+
+            return items;
+        }
+    }
+}
---
+++ EcommerceAPIs/EcommerceAPIs/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EcommerceAPIs.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Price { get; set; }
+        public string Memory { get; set; }
+        public string Company { get; set; }
+    }
+}
